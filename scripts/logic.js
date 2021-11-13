ca6234ea82ad649a0d765633bd8df7bf45291970
Muhammad Musaab <muhammadmussabulhaq@gmail.com>
2021-11-13 17:08:17 +0500

    removed tic tac toe implementaton

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,7 +1,5 @@
 choices = ['Scissor', 'Rock', 'Paper']
-playerGrid = [[-1,-1,-1],
-              [-1,-1,-1],
-              [-1,-1,-1]]
+
 function randomValueFromArray(array) {
     const random = Math.floor(Math.random()*array.length)
     return array[random]
@@ -20,17 +18,7 @@ function move(playerSelection, computerSelection) {
     
 }
 
-function errorCheck(array) {
-    let error = 'Restart the game'
-    if (array.length !== 3){
-        return error
-    }
-    for (let i = 0; i < array.length; i++) {
-        if (array[i].length !== 3){
-            return error
-        }
-    }
-}
+
 
 /**
  * Case 1 : Criss Cross
