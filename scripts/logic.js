78092bc99c6b72c802d6803263ffa0af000c8608
Muhammad Musaab <muhammadmussabulhaq@gmail.com>
2021-11-14 11:05:22 +0500

    changing local location of script to website location

--- scripts/logic.js
+++ scripts/logic.js
@@ -18,77 +18,94 @@ function playerSelection() {
 
 function move(playerSelection, computerSelection) {
     if (computerSelection == playerSelection) {
-        displayResults(`Tie game! ${computerSelection} tries to beat ${playerSelection}`);
-      } else if (
-        (computerSelection == "rock" && playerSelection == "scissors") ||
-        (computerSelection == "scissors" && playerSelection == "paper") ||
-        (computerSelection == "paper" && playerSelection == "rock")
-      ) {
+        displayResults(`Tie game!\n ${computerSelection} tries to beat ${playerSelection}`);
+      } else if (whoWon == false) {
         computerScore = ++computerScore;
-        if (computerScore === 1) {
-          displayResults(
-            `Oh no! You lost.
-            ${computerSelection} beats ${playerSelection}.`
-          );
-        } else if (computerScore === 2) {
-          displayResults(
-            `Arghh. ${(
-              computerSelection
-            )} beats ${playerSelection}. Give it another shot!`
-          );
-        } else if (computerScore === 3) {
-          displayResults(
-            `${(
-              computerSelection
-            )} beats ${playerSelection}. It's ok. You got this!!`
-          );
-        } else if (computerScore === 4) {
-          displayResults(
-            `Oh no. It's match point!! ${(
-              computerSelection
-            )} beats ${playerSelection}. Don't let us down!`
-          );
-        } else {
-          displayResults(`${computerSelection} beats ${playerSelection}`);
-        }
-      } else if(
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
-        ) {
+        computerMotivatedResults()
+      } else if(whoWon == true) {
         playerScore = ++playerScore;
-        if (playerScore === 1) {
-          displayResults(
-            `Lets go!!! You won.
-            ${playerSelection} beats ${computerSelection}.`
-          );
-        } else if (playerScore === 2) {
-          displayResults(
-            `You're pretty good at this. ${
-              playerSelection
-            } beats ${computerSelection}.`
-          );
-        } else if (playerScore === 3) {
-          displayResults(
-            `${
-              playerSelection
-            } beats ${computerSelection}! Has mankind found its savior??`
-          );
-        } else if (playerScore === 4) {
-          displayResults(
-            `${
-              playerSelection
-            } beats ${computerSelection}. One more and you're a hero!`
-          );
-        } else {
-          displayResults(`${playerSelection} beats ${computerSelection}`);
-        }
+        playerMotivatedResults()
       }
       else {
         alert('invalid option')
       }
     }
 
+/**returns true if its player, false if its not player */
+function whoWon(computerSelection,playerSelection) {
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+    ){
+    return true /**Player won */
+  }else if(
+    (computerSelection == "rock" && playerSelection == "scissors") ||
+    (computerSelection == "scissors" && playerSelection == "paper") ||
+    (computerSelection == "paper" && playerSelection == "rock")
+  ){
+    return false /**Computer won */
+  }
+}
+
+/**a function which does motivational quotes on the number of wins */
+function computerMotivatedResults(params) {
+  if (computerScore === 1) {
+    displayResults(
+      `Oh no! You lost.
+      ${computerSelection} beats ${playerSelection}.`
+    );
+  } else if (computerScore === 2) {
+    displayResults(
+      `Arghh. ${(
+        computerSelection
+      )} beats ${playerSelection}. Give it another shot!`
+    );
+  } else if (computerScore === 3) {
+    displayResults(
+      `${(
+        computerSelection
+      )} beats ${playerSelection}. It's ok. You got this!!`
+    );
+  } else if (computerScore === 4) {
+    displayResults(
+      `Oh no. It's match point!! ${(
+        computerSelection
+      )} beats ${playerSelection}. Don't let us down!`
+    );
+  } else {
+    displayResults(`${computerSelection} beats ${playerSelection}`);
+  }
+}
+
+function playerMotivatedResults() {
+  if (playerScore === 1) {
+    displayResults(
+      `Lets go!!! You won.
+      ${playerSelection} beats ${computerSelection}.`
+    );
+  } else if (playerScore === 2) {
+    displayResults(
+      `You're pretty good at this. ${
+        playerSelection
+      } beats ${computerSelection}.`
+    );
+  } else if (playerScore === 3) {
+    displayResults(
+      `${
+        playerSelection
+      } beats ${computerSelection}! Has mankind found its savior??`
+    );
+  } else if (playerScore === 4) {
+    displayResults(
+      `${
+        playerSelection
+      } beats ${computerSelection}. One more and you're a hero!`
+    );
+  } else {
+    displayResults(`${playerSelection} beats ${computerSelection}`);
+  }
+}
 
 function displayResults(string) {
     alert(string)
